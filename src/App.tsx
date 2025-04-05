b6d19911b6392f3846efa8a0dc195a9d2030c454
bluebaney10 <bluebaney10@gmail.com>
2025-04-05 20:41:41 +0700

    Adding List

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import ListGroup from "./components/ListGroup";
+import ListUser from "./components/ListUser";
 
 function App() {
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  const handleItem = (item: string) => {
+    console.log("item:", item);
+  };
+
   return (
     <>
-      <ListGroup />
+      <ListGroup items={items} onSelectItem={handleItem} />
+      <ListUser />
     </>
   );
 }
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,16 +1,36 @@
-function ListGroup() {
-  const item = ["Blue", "Baney"];
+import { useState } from "react";
+
+type Props = {
+  items: string[];
+  onSelectItem: (item: string) => void;
+};
+
+const ListGroup = ({ items, onSelectItem }: Props) => {
+  const [selectedIndex, setSelectedIndex] = useState(0);
 
   return (
     <>
       <h1>List</h1>
       <ul className="list-group">
-        {item.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
   );
-}
+};
 
 export default ListGroup;
