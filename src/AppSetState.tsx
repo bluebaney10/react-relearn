5e07f255e96b898f6d77efbd0d8a09802854f6e1
bluebaney10 <bluebaney10@gmail.com>
2025-04-16 22:44:03 +0700

    add app set state

--- src/AppSetState.tsx
+++ src/AppSetState.tsx
@@ -11,11 +11,26 @@ function AppSetState() {
     setCount2((prevcount) => prevcount + 1);
   };
 
+  const [firstName, setFirstName] = useState("Blue");
+  const [lastName, setLastName] = useState("Baney");
+
+  // Calculate fullName dynamically
+  const fullName = `${firstName} ${lastName}`;
+
   return (
     <>
       {count1} num of count 1<button onClick={increment1}>+</button>
       <br></br>
       {count2} num of count 2<button onClick={increment2}>+</button>
+      <br></br>
+      Hello, {fullName}
+      <button
+        onClick={() => {
+          setFirstName("Buncha");
+        }}
+      >
+        Change name
+      </button>
     </>
   );
 }
